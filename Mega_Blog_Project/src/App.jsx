893c631a56d8c66df601f8effd10e06fd0fa85be
Ayush <ayushsuryawanshi004@gmail.com>
2024-02-05 16:48:43 +0530

    started with downloading all dependencies

--- Mega_Blog_Project/src/App.jsx
+++ Mega_Blog_Project/src/App.jsx
@@ -1,33 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>A blog with appwrite</div>
     </>
   )
 }
@@ -39,5 +18,7 @@ export default App
 /* Appwrite - For using backend services kyuki hmne to abhi khali react pdha h to backend opensource se easily ho jaega
    Tiny MCE - For using text editor functionality, kyuki jo backend se html ka response aayega usko interpret krke frontend me show krna h
    React Hook Form - Ye apne forms ko manage krne ke liye hai, apn ne thoda advance form use kiye hai
-
+ 
+   npm commands to download project dependencies - (google me jaa ke dekh lena download krte time)
+    npm install @reduxjs/toolkit react-redux react-router-dom appwrite @tinymce/tinymce-react html-react-parserreact-hook-form
 */
