5f61fb743086d2bdfed172f1fa46203bab07f421
Ayush <ayushsuryawanshi004@gmail.com>
2024-02-05 16:35:03 +0530

    first commit

---
+++ Mega_Blog_Project/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
+
+
+// here we will use these opensource plateform:
+/* Appwrite - For using backend services kyuki hmne to abhi khali react pdha h to backend opensource se easily ho jaega
+   Tiny MCE - For using text editor functionality, kyuki jo backend se html ka response aayega usko interpret krke frontend me show krna h
+   React Hook Form - Ye apne forms ko manage krne ke liye hai, apn ne thoda advance form use kiye hai
+
+*/
